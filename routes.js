1665c647a60add8dc9fecf2c2da81a03e560fc22
abc <abc@example.com>
2019-06-07 15:04:21 +1200

    it works

--- routes.js
+++ routes.js
@@ -5,6 +5,12 @@ const fs = require("fs")
 
 const server = express();
 
+
+let moreCupcakes = {
+    cupcakes : data
+}
+
+
 router.get('/', (req, res) => {
     res.render('cupcakes/index', data)
 })
@@ -52,14 +58,33 @@ router.post('/cupcakes/edit/:id', (req, res) => {
     );
 })
 
+router.get('/cupcakes/add/new', (req, res) => {
+    
+    res.render("cupcakes/add");
+});
+
+//  router.post('/cupcakes/add/new', (req, res) => {
+//     req.body = 
+
+//     console.log(body)
+    // fs.appendFile(
+    //     "./data.json",
+    //     JSON.stringify(data, null, 2),
+    //     "utf8",
+    //     err => {
+    //         if (err) throw err;
+    //         console.log("The cupcakes have been updated");
+    //         res.redirect("/cupcakes/");
+
+    // })
+    router.post('/cupcakes/add/new', (req, res) => {
+        req.body.id = data.cupcakes.length + 1;
+        
+        data.cupcakes.push(req.body)
+        console.log(req.body)
+        fs.writeFile('data.json', JSON.stringify(data, null, 2), 'utf8', (err) => {
+            res.redirect('/cupcakes/' + req.body.id);
+        })
+     })
+
 module.exports = router
-
-
-
-
-
-// "breed": "Human 1",
-
-//  "_locals": {}
-
-
